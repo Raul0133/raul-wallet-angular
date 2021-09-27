929d43e49d25323389372566c83a9c2174b2c9a2
Raul <raullek03@gmail.com>
2021-09-27 19:10:40 -0300

    historico de cotações lidas

--- src/app/components/raul-currency/raul-currency.component.ts
+++ src/app/components/raul-currency/raul-currency.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RaulWalletService } from 'src/app/raul-wallet.service';
 
 @Component({
   selector: 'app-raul-currency',
@@ -6,10 +7,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./raul-currency.component.css']
 })
 export class RaulCurrencyComponent implements OnInit {
+  BRLAtual!: number
+  USDAtual!: number
+  EURAtual!: number
+  historicoBRL: number[] = []
+  historicoUSD: number[] = []
+  historicoEUR: number[] = []
 
-  constructor() { }
+  constructor(private raulWalletService: RaulWalletService) { }
 
   ngOnInit(): void {
+    this.getBRLBitcoin()
+    this.getUSDBitcoin()
+    this.getEURBitcoin()
+
+    this.interval()
   }
 
+  getBRLBitcoin() {
+    this.raulWalletService.getBRLBitcoin().subscribe((btc: any) => {
+      this.BRLAtual = btc.bpi.BRL.rate_float
+      this.historicoBRL.push(this.BRLAtual)
+    })
+  }
+
+  getUSDBitcoin() {
+    this.raulWalletService.getUSDBitcoin().subscribe((btc: any) => {
+      this.USDAtual = btc.bpi.USD.rate_float
+      this.historicoUSD.push(this.USDAtual)
+    })
+  }
+
+  getEURBitcoin() {
+    this.raulWalletService.getEURBitcoin().subscribe((btc: any) => {
+      this.EURAtual = btc.bpi.EUR.rate_float
+      this.historicoEUR.push(this.EURAtual)
+    })
+  }
+
+  interval() {
+    setInterval(() => {
+      this.getBRLBitcoin()
+      this.getUSDBitcoin()
+      this.getEURBitcoin()
+    }, 60000)
+  }
 }
